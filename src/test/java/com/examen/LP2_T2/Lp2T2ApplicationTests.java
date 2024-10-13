725db6205faa478620c8b97215e9e8cec2f4cf8a
Rosario <i202312066@cibertec.edu.pe>
2024-10-13 10:24:49 -0500

    Estructura del examen

---
+++ src/test/java/com/examen/LP2_T2/Lp2T2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.LP2_T2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
